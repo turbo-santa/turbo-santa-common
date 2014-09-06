71ab9e4e09e662e0a22128728aab22167d38537c
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-09-06 06:17:53 -0400

    added function declarations

--- back_end/opcode_handlers.h
+++ back_end/opcode_handlers.h
@@ -6,8 +6,27 @@ namespace back_end {
 namespace handlers {
 
 int Add8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
-
 int Add8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int ADC8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int ADC8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int Sub8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int Sub8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int And8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int And8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int Or8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int Or8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int Xor8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int Xor8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int Cp8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
+int Cp8BitLiteral(unsigned char* rom, int instruction_ptr, Opcode opcode);
+
+int Inc8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
 
 int Dec8Bit(unsigned char* rom, int instruction_ptr, Opcode opcode);
 
