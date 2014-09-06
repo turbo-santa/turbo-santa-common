e97c41263f59acd0cdb961ff2054b2cc931aba07
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-09-06 03:03:04 -0400

    created opcode parser

---
+++ back_end/opcode_parser.cc
@@ -0,0 +1,19 @@
+#include "back_end/opcodes.h"
+
+class OpcodeParser {
+    int instruction_ptr_;
+    char* rom_;
+  public: 
+    OpcodeParser(char* rom);
+    void ReadInstruction();
+};
+
+OpcodeParser::OpcodeParser(char* rom) {
+    rom_ = rom;
+}
+
+void OpcodeParser::ReadInstruction() {
+    char opcode = rom_[instruction_ptr_];
+    back_end::opcodes::Opcode opcode_struct = opcode_map[opcode];
+    instruction_ptr_ = opcode_struct.handler(instruction_ptr_, opcode_struct.length);
+}
