888c0e062235549d8f1c54dd7c50b8c62e162446
Brendan Higgins <bjh83@case.edu>
2014-09-18 00:33:38 -0400

    I started adding the new world memory segments.

--- back_end/memory/memory_segment.cc
+++ back_end/memory/memory_segment.cc
@@ -3,33 +3,33 @@
 namespace back_end {
 namespace memory {
 
-Predicate CreateIsInRange(unsigned short min, unsigned short max) {
-    return [min, max](unsigned short address) { return min <= address && address <= max; };
-}
-
-ReadAction CreateReadAction(MemorySegment* memory_segment) {
-    return [memory_segment](unsigned short address) { return memory_segment->get(address); };
-}
-
-WriteAction CreateWriteAction(MemorySegment* memory_segment) {
-    return [memory_segment](unsigned short address, unsigned char value) { return memory_segment->set(address, value); };
-}
-
-ReadDispatcher CreateReadDispatcher(unsigned short min, unsigned short max, MemorySegment* memory_segment) {
-    return { CreateIsInRange(min, max), CreateReadAction(memory_segment) };
-}
-
-WriteDispatcher CreateWriteDispatcher(unsigned short min, unsigned short max, MemorySegment* memory_segment) {
-    return { CreateIsInRange(min, max), CreateWriteAction(memory_segment) };
-}
-
-void ReadOnlyMemorySegment::set(unsigned short address, unsigned char value) {
-    for (WriteDispatcher dispatcher : dispatchers_) {
-        if (dispatcher.predicate(address)) {
-            dispatcher.action(address, value);
-        }
+void ROMSegment0::WriteHandler(unsigned short address, unsigned char value) {
+    if (0x0000 <= address && address <= 0x1fff) {
+        RAMBankEnable(value);
+    } else if (0x2000 <= address && address <= 0x2fff) {
+        ROMBankSelectLSByte(value);
+    } else if (0x3000 <= address && address <= 0x3fff) {
+        ROMBankSelectMSByte(value);
     }
 }
+
+void ROMSegment0::RAMBankEnable(unsigned char value);
+
+void ROMSegment0::ROMBankSelectLSByte(unsigned char value);
+
+void ROMSegment0::ROMBankSelectMSByte(unsigned char value);
+
+void ROMSegmentN::WriteHandler(unsigned short address, unsigned char value) {
+    if (0x4000 <= address && address <= 0x5fff) {
+        RAMBankSelect(value);
+    } else if (0x6000 <= address && address <= 0x7fff) {
+        RAMROMSelect(value);
+    }
+}
+
+void ROMSegmentN::RAMBankSelect(unsigned char value);
+
+void ROMSegmentN::RAMROMSelect(unsigned char value);
 
 } // namespace memory
 } // namespace back_end
--- back_end/memory/memory_segment.h
+++ back_end/memory/memory_segment.h
@@ -9,59 +9,84 @@ namespace memory {
 
 class MemorySegment {
     public:
-        virtual unsigned char get(unsigned short address) = 0;
-
-        virtual void set(unsigned short address, unsigned char value) = 0;
-};
-
-class TransparentMemorySegment : public MemorySegment {
-    public:
-        TransparentMemorySegment(unsigned char* raw_memory) : raw_memory_(raw_memory) {}
-
-        virtual unsigned char get(unsigned short address) {
-            return raw_memory_[address];
+        // Whether this is the memory segment that the address is in.
+        virtual bool InRange(unsigned short address) {
+            return lower_address_bound() <= address && address <= upper_address_bound();
         }
 
-        virtual void set(unsigned short address, unsigned char value) {
-            raw_memory_[address] = value;
-        }
+        // Read the value at this memory address.
+        virtual unsigned char Read(unsigned short address) = 0;
 
-    private:
-        unsigned char* raw_memory_;
-};
-
-typedef std::function<bool(unsigned short address)> Predicate;
-typedef std::function<void(unsigned short address, unsigned char value)> WriteAction;
-typedef std::function<unsigned char (unsigned short address)> ReadAction;
-
-struct ReadDispatcher {
-    Predicate predicate;
-    ReadAction action;
-};
-
-struct WriteDispatcher {
-    Predicate predicate;
-    WriteAction action;
-};
-
-Predicate CreateIsInRange(unsigned short min, unsigned short max);
-
-ReadAction CreateReadAction(MemorySegment* memory_segment);
-
-WriteAction CreateWriteAction(MemorySegment* memory_segment);
-
-ReadDispatcher CreateReadDispatcher(unsigned short min, unsigned short max, MemorySegment* memory_segment);
-
-WriteDispatcher CreateWriteDispatcher(unsigned short min, unsigned short max, MemorySegment* memory_segment);
-
-class ReadOnlyMemorySegment : public TransparentMemorySegment {
-    public:
-        ReadOnlyMemorySegment(unsigned char* raw_memory) : TransparentMemorySegment(raw_memory) {}
-
-        virtual void set(unsigned short address, unsigned char value);
+        // Write this value to this memory address.
+        virtual void Write(unsigned short address, unsigned char value) = 0;
 
     protected:
-        std::vector<WriteDispatcher> dispatchers_;
+        // Inclusive lower bound of this memory segment.
+        virtual unsigned short lower_address_bound() = 0;
+
+        // Inclusive upper bound of this memory segment.
+        virtual unsigned short upper_address_bound() = 0;
+};
+
+struct RawMemoryBlock {
+    unsigned char* raw_memory_start;
+    unsigned long size;
+};
+
+class ROMSegment : public MemorySegment {
+    public:
+        ROMSegment(
+                RawMemoryBlock* memory_block,
+                unsigned short lower_address_bound,
+                unsigned short upper_address_bound) 
+            : lower_address_bound_(lower_address_bound),
+            upper_address_bound_(upper_address_bound),
+            memory_block_(memory_block) {}
+
+        virtual unsigned char Read(unsigned short address) {
+            return memory_block_->raw_memory_start[translate_address(address)];
+        }
+
+        virtual void Write(unsigned short address, unsigned char value) {
+            WriteHandler(address, value);
+        }
+
+
+    protected:
+        virtual void WriteHandler(unsigned short address, unsigned char value) = 0;
+
+        virtual unsigned short lower_address_bound() { return lower_address_bound_; }
+        virtual unsigned short upper_address_bound() { return upper_address_bound_; }
+
+    private:
+        unsigned short translate_address(unsigned short address) { return address - lower_address_bound_; }
+        
+        unsigned short lower_address_bound_;
+        unsigned short upper_address_bound_;
+        RawMemoryBlock* memory_block_;
+};
+
+class ROMSegment0 : public ROMSegment {
+    public:
+        ROMSegment0(RawMemoryBlock* memory_block)
+            : ROMSegment(memory_block, 0x0000, 0x3fff) {}
+
+    protected:
+        virtual void WriteHandler(unsigned short address, unsigned char value);
+        virtual void RAMBankEnable(unsigned char value);
+        virtual void ROMBankSelectLSByte(unsigned char value);
+        virtual void ROMBankSelectMSByte(unsigned char value);
+};
+
+class ROMSegmentN : public ROMSegment {
+    public:
+        ROMSegmentN(RawMemoryBlock* memory_block)
+            : ROMSegment(memory_block, 0x4000, 0x7fff) {}
+
+    protected:
+        virtual void WriteHandler(unsigned short address, unsigned char value);
+        virtual void RAMBankSelect(unsigned char value);
+        virtual void RAMROMSelect(unsigned char value);
 };
 
 } // namespace memory
