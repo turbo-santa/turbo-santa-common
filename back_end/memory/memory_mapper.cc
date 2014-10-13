e30adf4630f3cb5ecd28c9a8611627612a51b47d
Brendan Higgins <bjh83@case.edu>
2014-10-13 18:28:33 -0400

    Patched the shit out of shit.

--- back_end/memory/memory_mapper.cc
+++ back_end/memory/memory_mapper.cc
@@ -3,40 +3,5 @@
 namespace back_end {
 namespace memory {
 
-MemoryMapper::MemoryMapper(unsigned char* raw_rom, long rom_size) 
-    : mapped_memory_(std::unique_ptr<unsigned char>(new unsigned char[0x10000])),
-    rom_bank_0_(mapped_memory_.get()),
-    rom_bank_1_(mapped_memory_.get() + kROMSegmentSize),
-    rom_bank_manager_(mapped_memory_.get() + kROMSegmentSize, kROMSegmentSize, kROMNumber),
-    ram_bank_0_(mapped_memory_.get() + 0xc000),
-    ram_bank_1_(mapped_memory_.get() + 0xd000),
-    ram_bank_manager_(mapped_memory_.get() + 0xd000, kRAMSegmentSize, kRAMNumber) {
-        int i = 0;
-        for (; i < rom_size; i++) {
-            mapped_memory_.get()[i] = raw_rom[i];
-            break;
-        }
-        for (; i < 0x10000; i++) {
-            mapped_memory_.get()[i] = 0;
-        }
-    }
-
-void MemoryMapper::set(unsigned short location, unsigned char value) {
-    for (WriteDispatcher dispatcher : write_dispatchers) {
-        if (dispatcher.predicate(location)) {
-            dispatcher.action(location, value);
-        }
-    }
-}
-
-unsigned char MemoryMapper::get(unsigned short location) {
-    for (ReadDispatcher dispatcher : read_dispatchers) {
-        if (dispatcher.predicate(location)) {
-            return dispatcher.action(location);
-        }
-    }
-    return 0;
-}
-
 } // namespace memory
 } // namespace back_end
--- back_end/memory/memory_mapper.h
+++ back_end/memory/memory_mapper.h
@@ -3,7 +3,6 @@
 
 #include "back_end/memory/memory_page.h"
 #include "back_end/memory/memory_segment.h"
-// #include "test_harness/test_harness.h"
 
 namespace back_end {
 namespace memory {
@@ -16,74 +15,24 @@ class ZeroPage;
 class InteruptEnable;
 
 class MemoryMapper {
-    public:
-        MemoryMapper(unsigned char* raw_rom, long rom_size);
+ public:
+  MemoryMapper() : mapped_memory_(new unsigned char[kMaxSize]) {}
 
-        unsigned char* get_pointer() { return mapped_memory_.get(); }
+  ~MemoryMapper() { delete mapped_memory_; }
 
-        void set(unsigned short location, unsigned char value);
+  unsigned char* get_pointer() { return mapped_memory_; }
+
+  void set(unsigned short location, unsigned char value) { mapped_memory_[location] = value; }
 
 
-        unsigned char get(unsigned short location);
+  unsigned char get(unsigned short location) { return mapped_memory_[location]; }
 
-        static const long kROMSegmentSize = 0x4000;
-        // TODO(Brendan): Put an actual size here.
-        static const int kROMNumber = 1;
-        static const long kRAMSegmentSize = 0x1000;
-        // TODO(Brendan): Put an actual size here.
-        static const int kRAMNumber = 1;
+  unsigned long kMaxSize = 0x10000;
 
-    private:
-        std::unique_ptr<unsigned char> mapped_memory_;
-        ReadOnlyMemorySegment rom_bank_0_;
-        ReadOnlyMemorySegment rom_bank_1_;
-        PageManager rom_bank_manager_;
-        TransparentMemorySegment ram_bank_0_;
-        TransparentMemorySegment ram_bank_1_;
-        PageManager ram_bank_manager_;
-//         VideoRam video_ram_;
-//         TransparentMemorySegment cartridge_ram_;
-//         ObjectAttributeMemory oam;
-//         Unusable unusable_;
-//         IORegisters i_o_registers_;
-//         ZeroPage zero_page_;
-//         InteruptEnable interupt_enable_;
-        std::vector<WriteDispatcher> write_dispatchers = {
-            CreateWriteDispatcher(0x0000, 0x3fff, &rom_bank_0_),
-            CreateWriteDispatcher(0x4000, 0x7fff, &rom_bank_1_),
-//             CreateWriteDispatcher(0x8000, 0x9fff, &video_ram_),
-//             CreateWriteDispatcher(0xa000, 0xbfff, &cartridge_ram_),
-            CreateWriteDispatcher(0xc000, 0xcfff, &ram_bank_0_),
-            CreateWriteDispatcher(0xd000, 0xdfff, &ram_bank_1_),
-            // Yeah, so the memory from 0xd000 to 0xdfff is the same memory
-            // as 0xe000 to 0xfdff.
-            CreateWriteDispatcher(0xe000, 0xfdff, &ram_bank_1_),
-//             CreateWriteDispatcher(0xfe00, 0xfe9f, &oam_),
-//             CreateWriteDispatcher(0xfea0, 0xfeff, &unusable_),
-//             CreateWriteDispatcher(0xff00, 0xff7f, &i_o_registers_),
-//             CreateWriteDispatcher(0xff80, 0xfffe, &zero_page_),
-//             CreateWriteDispatcher(0xffff, &interupt_enable_)
-        };
-
-        std::vector<ReadDispatcher> read_dispatchers = {
-            CreateReadDispatcher(0x0000, 0x3fff, &rom_bank_0_),
-            CreateReadDispatcher(0x4000, 0x7fff, &rom_bank_1_),
-//             CreateReadDispatcher(0x8000, 0x9fff, &video_ram_),
-//             CreateReadDispatcher(0xa000, 0xbfff, &cartridge_ram_),
-            CreateReadDispatcher(0xc000, 0xcfff, &ram_bank_0_),
-            CreateReadDispatcher(0xd000, 0xdfff, &ram_bank_1_),
-            // Yeah, so the memory from 0xd000 to 0xdfff is the same memory
-            // as 0xe000 to 0xfdff.
-            CreateReadDispatcher(0xe000, 0xfdff, &ram_bank_1_),
-//             CreateReadDispatcher(0xfe00, 0xfe9f, &oam_),
-//             CreateReadDispatcher(0xfea0, 0xfeff, &unusable_),
-//             CreateReadDispatcher(0xff00, 0xff7f, &i_o_registers_),
-//             CreateReadDispatcher(0xff80, 0xfffe, zero_page_),
-//             CreateReadDispatcher(0xffff, interupt_enable_)
-        };
-
-//         friend class test_harness::TestHarness;
+ private:
+  unsigned char* mapped_memory_;
 };
+
 } // namespace memory_mapper
 } // namespace back_end
 #endif // TURBO_SANTA_COMMON_BACK_END_MEMORY_MEMORY_MAPPER_H_
