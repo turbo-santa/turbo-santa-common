abf4a3f04fcc22d93795e60b670e8da086c1ca73
David Jannotta <dave@davidjannotta.com>
2014-10-18 16:32:28 -0400

    Added new tests, note: inc8 and dec8 compile but the tests fail

--- back_end/opcode_executor/opcode_handlers_test.cc
+++ back_end/opcode_executor/opcode_handlers_test.cc
@@ -38,13 +38,13 @@ TEST_F(OpcodeHandlersTest, Add8BitDirect) {
     EXPECT_EQ(0, get_instruction_ptr());
     AssertRegisterState({{Register::A, 0}, {Register::B, 0}});
     SetRegisterState({{Register::A, 1}, {Register::B, 2}});
-    AssertRegisterState({{Register::A, 1}, {Register::B, 2}});
+    EXPECT_REGISTER({{Register::A, 1}, {Register::B, 2}});
 
     get_rom_ptr()[get_instruction_ptr()] = 0x80;
     int actual_new_instruction_ptr = Add8Bit(get_rom_ptr(), get_instruction_ptr(), {0x80, nullptr, nullptr, nullptr});
     int expected_new_instruction_ptr = 1;
     EXPECT_EQ(expected_new_instruction_ptr, actual_new_instruction_ptr);
-    AssertRegisterState({{Register::A, 3}, {Register::B, 2}});
+    EXPECT_REGISTER({{Register::A, 3}, {Register::B, 2}});
 }
 
 TEST_F(OpcodeHandlersTest, GetRegisterValue) {
@@ -59,11 +59,74 @@ TEST_F(OpcodeHandlersTest, GetRegisterValue) {
 TEST_F(OpcodeHandlersTest, Cp8Bit) {
     SetRegisterState({{Register::A, 1}, {Register::B, 1}});
     ExecuteInstruction(0xb8);
-    AssertRegisterState({{Register::FZ, 1}, {Register::FN, 1}, {Register::FH, 0}, {Register::FC, 0}});
+    EXPECT_REGISTER({{Register::FZ, 1}, {Register::FN, 1}, {Register::FH, 0}, {Register::FC, 0}});
 
     SetRegisterState({{Register::A, 1}, {Register::B, 2}});
     ExecuteInstruction(0xb8);
-    AssertRegisterState({{Register::FZ, 0}, {Register::FN, 1}, {Register::FH, 1}, {Register::FC, 1}});
+    EXPECT_REGISTER({{Register::FZ, 0}, {Register::FN, 1}, {Register::FH, 1}, {Register::FC, 1}});
+}
+
+TEST_F(OpcodeHandlersTest, Sub8Bit) {
+    SetRegisterState({{Register::A, 2}, {Register::B, 1}});
+    EXPECT_EQ(0, get_instruction_ptr());
+    ExecuteInstruction(0x90);
+    EXPECT_EQ(1, get_instruction_ptr());
+    EXPECT_REGISTER({{Register::A, 1}, {Register::B, 1}});
+    EXPECT_EQ(1, get_instruction_ptr());
+}
+
+TEST_F(OpcodeHandlersTest, And8Bit) {
+    SetRegisterState({{Register::A, 1}, {Register::B, 2}});
+    ExecuteInstruction(0xA0);
+    EXPECT_REGISTER({{Register::A, 0}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 2}, {Register::B, 2}});
+    ExecuteInstruction(0xA0);
+    EXPECT_REGISTER({{Register::A, 2}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 0}, {Register::B, 0}});
+    ExecuteInstruction(0xA0);
+    EXPECT_REGISTER({{Register::A, 0}, {Register::B, 0}});
+}
+
+TEST_F(OpcodeHandlersTest, Or8Bit) {
+    SetRegisterState({{Register::A, 1}, {Register::B, 2}});
+    ExecuteInstruction(0xB0);
+    EXPECT_REGISTER({{Register::A, 3}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 2}, {Register::B, 2}});
+    ExecuteInstruction(0xB0);
+    EXPECT_REGISTER({{Register::A, 2}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 1}, {Register::B, 0}});
+    ExecuteInstruction(0xB0);
+    EXPECT_REGISTER({{Register::A, 1}, {Register::B, 0}});
+}
+
+TEST_F(OpcodeHandlersTest, Xor8Bit) {
+    SetRegisterState({{Register::A, 1}, {Register::B, 2}});
+    ExecuteInstruction(0xA8);
+    EXPECT_REGISTER({{Register::A, 3}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 2}, {Register::B, 2}});
+    ExecuteInstruction(0xA8);
+    EXPECT_REGISTER({{Register::A, 0}, {Register::B, 2}});
+
+    SetRegisterState({{Register::A, 0}, {Register::B, 0}});
+    ExecuteInstruction(0xA8);
+    EXPECT_REGISTER({{Register::A, 0}, {Register::B, 0}});
+}
+
+TEST_F(OpcodeHandlersTest, INC8Bit) {
+    SetRegisterState({{Register::A, 1}});
+    ExecuteInstruction(0x04);
+    EXPECT_REGISTER({{Register::A, 2}});
+}
+
+TEST_F(OpcodeHandlersTest, Dec8Bit) {
+    SetRegisterState({{Register::A, 1}});
+    ExecuteInstruction(0x05);
+    EXPECT_REGISTER({{Register::A, 0}});
 }
 
 } // namespace handlers
