2f24bbbd3cda20b0b94e01e6b7da429fe5606037
Brendan Higgins <bjh83@case.edu>
2014-11-19 06:56:30 -0500

    Fixed memory allocation error in ram_segment.

--- back_end/memory/ram_segment.h
+++ back_end/memory/ram_segment.h
@@ -11,14 +11,14 @@ class RAMSegment : public ContiguousMemorySegment {
   RAMSegment(unsigned short lower_address_bound, unsigned short upper_address_bound) :
       lower_address_bound_(lower_address_bound), 
       upper_address_bound_(upper_address_bound), 
-      memory_(upper_address_bound - lower_address_bound, 0x00) {}
+      memory_(upper_address_bound - lower_address_bound + 1, 0x00) {}
 
   virtual unsigned char Read(unsigned short address) {
-    return memory_[address];
+    return memory_[address - lower_address_bound_];
   }
 
   virtual void Write(unsigned short address, unsigned char value) {
-    memory_[address] = value;
+    memory_[address - lower_address_bound_] = value;
   }
 
  protected:
