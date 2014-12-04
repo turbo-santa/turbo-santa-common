711a15b1c605be452cc07c202f0c566616f0bcfa
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-12-03 20:57:06 -0500

    now use unique_ptrs

--- back_end/clocktroller/clocktroller_test.cc
+++ back_end/clocktroller/clocktroller_test.cc
@@ -9,14 +9,15 @@
 
 namespace back_end {
 namespace clocktroller {
-
+    
 using Register = test_harness::RegisterNameValuePair;
 using handlers::OpcodeExecutor;
 using test_harness::TestHarness;
 using test_harness::TestROM;
+using std::unique_ptr;
 using std::vector;
 using memory::MBC;
-
+  
 void ClocktrollerTest::LoadROM(const vector<TestROM>& test_rom) {
   MBC* mbc = parser_->memory_mapper_.mbc_.get();
   for (const TestROM& segment : test_rom) {
@@ -27,63 +28,62 @@ void ClocktrollerTest::LoadROM(const vector<TestROM>& test_rom) {
     }
   }
 }
-
-TEST_F(ClocktrollerTest, ExecuteInstructions) {
-    LOG(INFO) << "Setting register state";
-    SetRegisterState({{Register::A, 1}, {Register::L, 5}, {Register::H, 3}});
-    // add l to a
-    // add 8 bit literal to a
-    // sub h from a
-    LOG(INFO) << "Creating rom";
-    LoadROM({{0x0000, {
-        0x85,       // ADD A, L
-         0xC6, 0x04, // ADD A, 0x04
-        0x94}       // SUB H
-    }});
   
-    // TODO(Diego): Do not do this!!!! This should be wrapped in a unique_ptr!!!
-    Clocktroller* clocktroller = new Clocktroller(nullptr, 3);
-    LOG(INFO) << "Created new clocktroller";
-    clocktroller->executor = parser_;
-    LOG(INFO) << "Set OpcodeExecutor";
-    clocktroller->Setup();
-    LOG(INFO) << "Clocktroller set up";
-    clocktroller->Start();
-    LOG(INFO) << "Launched";
-    clocktroller->WaitForThreads();
-    EXPECT_REGISTER({{Register::A, 7}});
+TEST_F(ClocktrollerTest, ExecuteInstructions) {
+  LOG(INFO) << "Setting register state";
+  SetRegisterState({{Register::A, 1}, {Register::L, 5}, {Register::H, 3}});
+  // add l to a
+  // add 8 bit literal to a
+  // sub h from a
+  LOG(INFO) << "Creating rom";
+  LoadROM({{0x0000, {
+    0x85,       // ADD A, L
+    0xC6, 0x04, // ADD A, 0x04
+    0x94}       // SUB H
+  }});
+  
+  unique_ptr<Clocktroller> clocktroller(new Clocktroller(nullptr, 3));
+  LOG(INFO) << "Created new clocktroller";
+  clocktroller->executor = parser_;
+  LOG(INFO) << "Set OpcodeExecutor";
+  clocktroller->Setup();
+  LOG(INFO) << "Clocktroller set up";
+  clocktroller->Start();
+  LOG(INFO) << "Launched";
+  clocktroller->WaitForThreads();
+  EXPECT_REGISTER({{Register::A, 7}});
 }
 
 TEST_F(ClocktrollerTest, DISABLED_InstructionRuntime) {
-    SetRegisterState({{Register::A, 1}, {Register::B, 1}});
-    // add B to A repeatedly
-    // each add should take 4 clock cycles
-    // 8388000 cycles/sec
-    float clock_cycles = 4;
-    int numInstructions = 30000;
-    std::vector<unsigned char> instructions;
-    for (int i = 0; i < numInstructions; i++) {
-        instructions.push_back(0x80); // add b to a takes 4 clockcycles
-    }
-    LOG(INFO) << "Creating rom";
-    LoadROM({{0x0000, instructions}});
-    
-    Clocktroller* clocktroller = new Clocktroller(nullptr, numInstructions);
-    LOG(INFO) << "Created new clocktroller";
-    clocktroller->executor = parser_;
-    LOG(INFO) << "Set OpcodeExecutor";
-    clocktroller->Setup();
-    LOG(INFO) << "Clocktroller set up";
-    clock_t start = clock();
-    clocktroller->Start();
-    LOG(INFO) << "Launched";
-    clocktroller->WaitForThreads();
-    clock_t stop = clock();
-
-    EXPECT_EQ(((float)clock_cycles * numInstructions) /  8388000, ((float)stop - start) / CLOCKS_PER_SEC);
+  SetRegisterState({{Register::A, 1}, {Register::B, 1}});
+  // add B to A repeatedly
+  // each add should take 4 clock cycles
+  // 8388000 cycles/sec
+  float clock_cycles = 4;
+  int numInstructions = 30000;
+  std::vector<unsigned char> instructions;
+  for (int i = 0; i < numInstructions; i++) {
+    instructions.push_back(0x80); // add b to a takes 4 clockcycles
+  }
+  LOG(INFO) << "Creating rom";
+  LoadROM({{0x0000, instructions}});
+  
+  unique_ptr<Clocktroller> clocktroller(new Clocktroller(nullptr, numInstructions));
+  LOG(INFO) << "Created new clocktroller";
+  clocktroller->executor = parser_;
+  LOG(INFO) << "Set OpcodeExecutor";
+  clocktroller->Setup();
+  LOG(INFO) << "Clocktroller set up";
+  clock_t start = clock();
+  clocktroller->Start();
+  LOG(INFO) << "Launched";
+  clocktroller->WaitForThreads();
+  clock_t stop = clock();
+  
+  EXPECT_EQ(((float)clock_cycles * numInstructions) /  8388000, ((float)stop - start) / CLOCKS_PER_SEC);
 }
-
-
+  
+    
 } // namespace clocktroller
 } // namespace back_end
 
