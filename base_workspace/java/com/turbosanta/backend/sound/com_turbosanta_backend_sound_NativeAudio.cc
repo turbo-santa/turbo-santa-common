ae1db5b31aa3ec2c2c3b353f6d81bb4c522d9ebe
Brendan Higgins <bhig93@gmail.com>
2015-12-02 23:40:30 -0800

    Fixed shit.

--- base_workspace/java/com/turbosanta/backend/sound/com_turbosanta_backend_sound_NativeAudio.cc
+++ base_workspace/java/com/turbosanta/backend/sound/com_turbosanta_backend_sound_NativeAudio.cc
@@ -7,19 +7,14 @@
 using ::java_com_turbosanta_backend::sound::SoundPlayer;
 using ::java_com_turbosanta_backend::setHandle;
 
-static JavaVM* jvm;
-
 /*
  * Class:     com_turbosanta_backend_sound_NativeAudio
  * Method:    init
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_com_turbosanta_backend_sound_NativeAudio_init(JNIEnv *env, jobject obj) {
+void Java_com_turbosanta_backend_sound_NativeAudio_init(JNIEnv *env, jobject obj) {
+  JavaVM* jvm;
+  env->GetJavaVM(&jvm);
   SoundPlayer* handle = new SoundPlayer(jvm, env, obj);
   setHandle<SoundPlayer>(env, obj, handle);
-}
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* jvm_, void*) {
-  jvm = jvm_;
-  return JNI_VERSION_1_8;
 }
